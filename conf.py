9ca7fb8bb47f015c7e9977537c6b89bf9c070098
root <root@vps758893.ovh.net>
2019-12-10 15:43:38 +0100

    a

--- conf.py
+++ conf.py
@@ -1,4 +1,7 @@
 import json
+
+MY_IP="51.91.252.29"
+
 f=open("data/ipfs/config", "r")
 contents =f.read()
 f.close()
@@ -12,13 +15,13 @@ data["API"] = {"HTTPHeaders": {
     "POST"
   ],
  "Access-Control-Allow-Origin": [
-    "http://51.91.252.29:5001",
+    "http://"+MY_IP+":5001",
     "http://127.0.0.1:5001",
     "https://webui.ipfs.io"
   ]
   }
 }
-data["Bootstrap"] = ["/ip4/51.91.252.29/tcp/4001/ipfs/" + peer]
+data["Bootstrap"] = ["/ip4/"+MY_IP+"/tcp/4001/ipfs/" + peer]
 f = open("data/ipfs/config","w+")
 f.write(json.dumps(data, sort_keys=True, indent=4))
 f.close()
